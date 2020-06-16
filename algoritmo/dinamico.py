00dfaeea888453e5b7ba65d68f870cbcd795596b
gseva <gavrilovseva@gmail.com>
2020-06-15 21:57:52 -0300

    Agrego una heuristica al dinamico

--- algoritmo/dinamico.py
+++ algoritmo/dinamico.py
@@ -1,14 +1,14 @@
 
 import sys
 
-# taken from: https://codereview.stackexchange.com/questions/1526/finding-all-k-subset-partitions
+# Algoritmo que devuelve todos los k-subsets posibles de un set de elementos
+# Agarrado de: https://codereview.stackexchange.com/questions/1526/finding-all-k-subset-partitions
 def subsets_k(collection, k):
     yield from partition_k(collection, k, k)
 
-
 def partition_k(collection, min, k):
     if len(collection) == 1:
-        yield [collection]
+        yield [ collection ]
         return
 
     first = collection[0]
@@ -22,32 +22,58 @@ def partition_k(collection, min, k):
         if len(smaller) < k: yield [ [ first ] ] + smaller
 
 
+def bucket_by_value(elements, k):
+    """
+    Separa el conjunto de elementos en k partes con la misma cantidad de
+    elementos. El elemento se asigna a cada parte basado en su valor de mayor
+    a menor.
+    """
+    result = [[] for _ in range(k)]
+    items = reversed(sorted(elements.items(), key=lambda x: x[1]))
+    for i, (el, _) in enumerate(items):
+        result[i%k].append(el)
+    return result
+
+
 def dinamico(codigos_postales, cajas, n_destinos, t_caja, t_setup):
+    config_times = {}
 
     def posibles_divisiones(lote):
-        return list(subsets_k(list(lote.keys()), n_destinos))
+        return subsets_k(list(lote.keys()), n_destinos)
 
     def calcular_tiempo(lote):
         return t_setup + sum(lote.values()) * t_caja
 
     def procesar_recursivo(lote):
         t_actual = calcular_tiempo(lote)
-        divisiones = posibles_divisiones(lote)
 
-        if len(divisiones) == 1:
+        if len(lote) <= n_destinos:
             return t_actual
+
+        if len(lote) > 12:
+            # Heuristica para bajar la complejidad del algoritmo
+            divisiones = [bucket_by_value(lote, n_destinos)]
+        else:
+            divisiones = posibles_divisiones(lote)
 
         t_min = sys.maxsize  # Valor muy grande
         for division in divisiones:
             t_division = 0
             for config in division:
-                if len(config) > 1:
-                    sub_lote = {k: lote[k] for k in config}
-                    t_division += procesar_recursivo(sub_lote)
+                tup_config = tuple(config)
+                if len(tup_config) > 1:
+                    if tup_config not in config_times:
+                        sub_lote = {k: lote[k] for k in tup_config}
+                        config_times[tup_config] = procesar_recursivo(sub_lote)
+                    t_division += config_times[tup_config]
             if t_division < t_min:
                 t_min = t_division
         return t_actual + t_min
 
     primer_lote = dict(zip(codigos_postales, cajas))
+
+    for k, v in primer_lote.items():
+        config_times[tuple(k)] = v
+
     primer_config = codigos_postales
     return procesar_recursivo(primer_lote)
